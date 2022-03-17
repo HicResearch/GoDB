1ed408f0f751fae9347a82a6ea0351ad87523ff1
Chris Cole <c.cole@dundee.ac.uk>
2022-03-17 16:12:17 +0000

    adding tests for gd vcf file

---
+++ lib/py/test/vcfrecord_gd_test.py
@@ -0,0 +1,41 @@
+from re import S
+import sys
+sys.path.insert(1, 'lib/py')
+import vcfrecord
+
+def get_vcf_line(num = 1):
+    with open('lib/py/test/gd.vcf','r') as vcf:
+        n = 0     
+        for line in vcf:
+            line = line.strip()
+            if (line.startswith('#')):
+                pass
+            else:
+                n = n + 1
+                if (n == num):
+                    return(line)
+
+def test_get_chr():
+    vcfr = vcfrecord.VCFrecord(get_vcf_line(1))
+    chr = vcfr.get_chr()
+    assert chr == '21'
+
+def test_get_info_af():
+    vcfr = vcfrecord.VCFrecord(get_vcf_line(1))
+    af = vcfr.get_info_value('RefPanelAF')
+    assert af == '0.00092393'
+
+def test_get_info_info1():
+    vcfr = vcfrecord.VCFrecord(get_vcf_line(1))
+    info = vcfr.get_info_value('INFO')
+    assert info == '0.844373'
+
+def test_get_info_info1():
+    vcfr = vcfrecord.VCFrecord(get_vcf_line(2))
+    info = vcfr.get_info_value('INFO')
+    assert info == '1'
+
+def test_get_info_no_ER2():
+    vcfr = vcfrecord.VCFrecord(get_vcf_line(1))
+    info = vcfr.get_info_value('ER2')
+    assert info is None
